d76797d921e6faf464dc48d0694744f0b0b122a5
vscicchi <128091367+vscicchi@users.noreply.github.com>
2025-02-13 21:40:03 +0000

    Compile Testing Stuff

--- test.kt
+++ test.kt
@@ -1,3 +1,11 @@
-fun main() {
-  println("Hi!")
+// Purchase Categories: Dining, Leisure, Utilities, Rent, Shopping, Miscellaneous
+// Transaction Type: Cash, Credit, Debit, Check, Mobile
+
+fun logPurchase() {
+    // Read Date
+    // Read Name
+    // Read Amount
+    // Read Purchase Category
+    // Read Transaction Type
+    // Write to data.txt
 }
