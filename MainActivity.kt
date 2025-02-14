a903e12e7a55ab915c7c386377a635e326ec6f01
Joe Hicks <103856984+BeansAreNeat@users.noreply.github.com>
2025-02-13 19:12:48 -0500

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,60 @@
+package com.budget.budget
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.budget.budget.ui.theme.BudgetTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BudgetTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+
+    fun openLogSection(view: android.view.View) {
+        startActivity(Intent(this, LogPurchase::class.java))
+    }
+
+    fun openGraphs(view: android.view.View) {
+        //startActivity(Intent(this, ViewGraphs::class.java))
+    }
+
+    fun openTemplates(view: android.view.View) {
+        //startActivity(Intent(this, TemplatesActivity::class.java))
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BudgetTheme {
+        Greeting("Android")
+    }
+}
