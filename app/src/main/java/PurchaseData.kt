e0f3847540f662e6c33efcaa4b03db6e5cd8854a
CEOofBeans <103856984+CEOofBeans@users.noreply.github.com>
2025-03-04 22:07:04 -0500

    Just work already

--- app/src/main/java/PurchaseData.kt
+++
@@ -1,9 +0,0 @@
-package com.budget.budget
-
-data class PurchaseData(
-    val date: String,
-    val vendor: String,
-    val amount: Double,
-    val category: String,
-    val paymentType: String
-)
